71822f2d9d4bc9735c028fb3ae5eabca3d22c89e
bunnyrdy <121040061+bunnyrdy@users.noreply.github.com>
2026-01-27 21:47:56 +0530

    varibales and args

---
+++ os-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Defining a variable
+
+echo "Please enter your name:"
+read NAME
+echo "please enter password:"
+read PASSWORD
+echo "Welcome $NAME"
+echo "Your password is $PASSWORD"
