9bcc15885ee24011a73ee220abc0743521bc5e70
bunnyrdy <121040061+bunnyrdy@users.noreply.github.com>
2026-01-30 21:20:51 +0530

    updated loop program

--- loops.sh
+++ loops.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 USERID=$(id -u)
+TIMESTAMP=$(edate +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d. -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[0;31m"
+G="\e[0;32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 ...FAILURE
+        exit 1
+    else
+        echo "$2 ...SUCCESS
+    fi
+}
 
 if [ $USERID -ne 0 ]
 then
@@ -12,5 +28,13 @@ fi
 for i in $@
 do
     echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [$? -eq 0 ]
+    then
+        echo "already installed: $i ... skipping"
+    else
+        echo "not installed .... need to install: $i"
+    fi
+
 done
  
