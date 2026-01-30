a324d2c219191c51023680134d07ff5190c18abe
bunnyrdy <121040061+bunnyrdy@users.noreply.github.com>
2026-01-30 20:55:12 +0530

    loops example

---
+++ loops.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+USERID = $(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run as root user"
+    exit 1
+else
+    echo "you are root user, proceed with installations"
+fi  
+
+for i in $@
+do
+    echo "package to install: $i"
+done
+ 
