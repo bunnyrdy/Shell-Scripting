53f34b8992ceb7a181904f79d22df1718c7421ec
bunnyrdy <121040061+bunnyrdy@users.noreply.github.com>
2026-02-03 22:15:28 +0530

    monitoring

--- diskusage.sh
+++ diskusage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DISK_USAGE=$(DF -hT | grep xfs)
+DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print$6F}' | cut -d "%" -f1 )
-    FOLDER=$(echo $line | awk -F " " '{print$NF}')
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         echo "$FOLDER is more then $DISK_THRESHOLD, current usage: $USAGE"
