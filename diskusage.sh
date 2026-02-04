56762a81c4f894209b62a3816f40d33eaf338f33
bunnyrdy <121040061+bunnyrdy@users.noreply.github.com>
2026-02-04 20:55:10 +0530

    mailcode

--- diskusage.sh
+++ diskusage.sh
@@ -9,7 +9,13 @@ do
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        echo "$FOLDER is more then $DISK_THRESHOLD, current usage: $USAGE"
+        MESSAGE+=echo "$FOLDER is more then $DISK_THRESHOLD, current usage: $USAGE \n"
     fi
 
 done <<< $DISK_USAGE
+    echo -e "message: $MESSAGE"
+
+echo "$MESSAGE" | mail -s "Disk usage alert" srinivasrdy1011@gmail.com
+
+
+   
